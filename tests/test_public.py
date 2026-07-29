3ea0d95212104f9edb5a2c13246d2dc59f2a81f3
Will Waldron <Will.Waldron@uah.edu>
2026-07-29 13:42:06 -0500

    fix: matched public tests in assignment script to test script

--- tests/test_public.py
+++ tests/test_public.py
@@ -11,7 +11,8 @@ def test_problem1_shapes_and_values():
     assert len(x) == 100, "x must contain 100 elements"
     assert np.isclose(x[0], 0.0), "x must start at 0"
     assert np.isclose(x[-1], 2 * np.pi), "x must end at 2π"
-    assert np.allclose(y, np.sin(x)), "y must equal sin(x)"
+    assert np.allclose(y[0], 0), "y[0] is not close to 0"
+    assert np.allclose(y[-1], 0), "y[-1] is not close to 0"
 
 
 def test_problem2_value_and_error():
