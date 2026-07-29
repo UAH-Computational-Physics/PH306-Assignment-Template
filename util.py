79640ce7edcad3f4569af12e95b446e27f613671
Will Waldron <Will.Waldron@uah.edu>
2026-07-29 13:27:14 -0500

    Switched back to built-in assert

--- util.py
+++ util.py
@@ -3,15 +3,3 @@
 """
 
 # --- Functions --- #
-def assert_expr(expr: bool, msg: str) -> None:
-    """Assert that an expression is True, otherwise raise an AssertionError with a message.
-
-    Args:
-        expr (bool): The expression to evaluate.
-        msg (str): The message to display if the assertion fails.
-
-    Raises:
-        AssertionError: If the expression is False.
-    """
-    if __debug__ and not expr:
-        raise AssertionError(msg)
